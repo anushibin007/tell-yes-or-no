d8ab8b81a66114eb536f98c64b6191cb168d0130
Anu Shibin <anushibin007@gmail.com>
2021-07-13 23:01:48 +0530

    Better responsive loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { loadProgressBar } from "axios-progress-bar";
-import { Container, Row } from "react-bootstrap";
+import { Container, Spinner, Row } from "react-bootstrap";
 import "axios-progress-bar/dist/nprogress.css";
 
 function App() {
 	const API_URI = "https://yesno.wtf/api";
 
 	const [data, setData] = useState({});
+	const [loader, setLoader] = useState(true);
 
 	useEffect(() => {
 		fetchData();
@@ -16,8 +17,15 @@ function App() {
 
 	const fetchData = () => {
 		axios.get(API_URI).then((response) => {
+			setLoader(true);
+			setData({});
 			setData(response.data);
 		});
+	};
+
+	const imageLoaded = () => {
+		setLoader(false);
+		console.log("image loaded");
 	};
 
 	return (
@@ -25,7 +33,16 @@ function App() {
 			<Row className="justify-content-center">
 				<h1>{data.answer && data.answer.toUpperCase()}</h1>
 			</Row>
-			<Row className="justify-content-center align-items-stretch">{data.image && <img id="yes-no-image" alt={`${data.answer}-image`} src={data.image} />}</Row>
+			{data.image && (
+				<Row className="justify-content-center">
+					<img onLoad={imageLoaded} id="yes-no-image" alt={`${data.answer}-image`} src={data.image} />
+				</Row>
+			)}
+			{loader && (
+				<Row className="justify-content-center">
+					<Spinner animation="grow" variant="primary" />
+				</Row>
+			)}
 		</Container>
 	);
 }
